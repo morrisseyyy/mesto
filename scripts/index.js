f42b5a29dedb9f4b29ac3848c90a0d95d23a4e6b
morrisseyyy <nvsyromyatnikov@yandex.ru>
2023-05-16 01:18:27 +0700

    fix: Исправить ошибки в вёрстке и в js

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,8 @@ let formElement = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-let nameInput = document.querySelectorAll('.popup__input')[0];
-let jobInput = document.querySelectorAll('.popup__input')[1];
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
 
 function popupToToggle() {
     popup.classList.toggle('popup_opened');
@@ -16,13 +16,17 @@ function popupToToggle() {
     jobInput.value = profileDescription.textContent;
 }
 
-editButton.addEventListener('click', () => popupToToggle(popup));
+editButton.addEventListener('click', () => popupToToggle());
 
-closePopupButton.addEventListener('click', () => popupToToggle(popup));
+closePopupButton.addEventListener('click', () => popupToToggle());
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 
 popup.addEventListener('click', (event) => {
     if (event.target === event.currentTarget) {
-        popupToToggle(popup)
+        closePopup()
     }
 })
 
@@ -30,6 +34,7 @@ function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
+    closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
